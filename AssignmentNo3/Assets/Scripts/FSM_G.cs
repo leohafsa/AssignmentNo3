b611be0ce05648fbad0f7058c3321a4d7a6327d9
leohafsa <fa19-rcs-021@student.comsats.edu.pk>
2020-06-07 19:41:46 +0500

    done
    done

---
+++ AssignmentNo3/Assets/Scripts/FSM_G.cs
@@ -0,0 +1,79 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FSM_G: MonoBehaviour
+
+{
+    public enum events
+    {
+        TO_MOVE, Banana, Iron_tablet, Vitamin, Bread, poision, Remaining_life, No_life_left, New_Game
+    };
+    public enum states { Start_game, Dead, Search_food, Levelup_yellow, Powerup_green, Healthy,
+        leveldown, Gameover };
+
+    public states CurrentState
+    {
+        get
+        {
+            return currentState;
+        }
+        set
+        {
+            currentState = value;
+
+
+            StopAllCoroutines();
+
+            switch (currentState)
+            {
+                case states.Start_game:
+                    StartCoroutine(startgame());
+                    break;
+                case states.Search_food:
+                    StartCoroutine(searchfood());
+                    break;
+                case states.Powerup_green:
+                    StartCoroutine(powerup());
+                    break;
+                case states.Levelup_yellow:
+                    StartCoroutine(levelup());
+                    break;
+                case states.leveldown:
+                    StartCoroutine(leveldown());
+                    break;
+                case states.Healthy:
+                    StartCoroutine(healthy());
+                    break;
+                case states.Dead:
+                    StartCoroutine(dead());
+                    break;
+                case states.Gameover:
+                    StartCoroutine(gameover());
+                    break;
+
+
+
+            }
+        }
+    }
+
+        
+        void start()
+    {
+                CurrentState = states.Start_game;
+            }
+
+
+    public IEnumerator startgame()
+    {
+        while (currentState == states.Start_game)
+        {
+
+
+
+        }
+    }
+
+
+    
---
+++ AssignmentNo3/Assets/Scripts/walkanimContro.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class walkanimContro : MonoBehaviour
+{
+    static Animator anim;
+    public float Speed = 2.0f;
+   
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Transform ThisTransform = GetComponent<Transform>();
+
+        ThisTransform.position += new Vector3(Speed * Time.deltaTime, 0f, 0f);
+    }
+}
